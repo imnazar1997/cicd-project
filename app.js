a525407154f3dd17bac9bff5c5311a6db7d0c15d
imnazar1997 <nibrahim071@gmail.com>
2026-05-26 14:34:37 +0000

    sixth commit CICD pipeline

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const http = require('http');
 
 http.createServer((req, res) => {
-  res.write("CI/CD PipeLine Automatically working via webhook without click anything");
+  res.write("CI/CD PipeLine Automatically working via webhook without click anything after added webhook url in jenkins");
   res.end();
 }).listen(3000);
 
