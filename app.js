060179521c89d2f29e6909d15d853fa32953cb48
imnazar1997 <nibrahim071@gmail.com>
2026-05-26 15:50:28 +0000

    Nineth commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const http = require('http');
 
 http.createServer((req, res) => {
-  res.write("after installed plugin PipeLine Automatically working via webhook without click anything after added webhook url in jenkins");
+  res.write("Some changes done , after installed plugin PipeLine Automatically working via webhook without click anything after added webhook url in jenkins");
   res.end();
 }).listen(3000);
 
