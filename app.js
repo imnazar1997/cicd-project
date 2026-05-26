6fcc9d876a8d698e12f2c1779a03dae161f46612
imnazar1997 <nibrahim071@gmail.com>
2026-05-26 10:56:31 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+
+const http = require('http');
+
+http.createServer((req, res) => {
+  res.write("CI/CD Project Running!");
+  res.end();
+}).listen(3000);
+
+console.log("Server running on port 3000");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
